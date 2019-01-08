260823c8a5161931386b02a600b9dbe85e4d91e3
HackerRank <hackerrank@example.com>
2019-01-08 15:36:23 +0000

    Saved at Tue Jan 08 2019 13:36:23 GMT-0200 (Brasilia Summer Time)

--- src/main/java/com/hackerrank/github/model/Repo.java
+++ src/main/java/com/hackerrank/github/model/Repo.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Repo {
 
 	@Id
-    @GeneratedValue(strategy=GenerationType)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
 	private String name;
